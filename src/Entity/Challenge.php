8f740c1bf31bffef5f7a2b6c92a15084fb63dd85
François <franvoisoder@gmail.com>
2020-02-06 18:47:57 +0100

    Unicités sur les entités

--- src/Entity/Challenge.php
+++ src/Entity/Challenge.php
@@ -5,9 +5,11 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ChallengeRepository")
+ * @UniqueEntity("name")
  */
 class Challenge
 {
--- src/Entity/Validation.php
+++ src/Entity/Validation.php
@@ -3,9 +3,11 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ValidationRepository")
+ * @UniqueEntity({"createdBy", "challenge"})
  */
 class Validation
 {
