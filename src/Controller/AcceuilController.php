f3e628f38a5be94ac274e20d7a7eee40bd9dc703
François <franvoisoder@gmail.com>
2020-02-13 19:53:21 +0100

    Registration form

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AcceuilController extends AbstractController
@@ -14,6 +17,29 @@ class AcceuilController extends AbstractController
     {
         return $this->render('acceuil/index.html.twig', [
             'controller_name' => 'AcceuilController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/register", name="registration")
+     */
+    public function registration(Request $request){
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $user->setRoles(["ROLE_USER"])
+                ->setScore(0);
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($user);
+            $manager->flush();
+        }
+
+        return $this->render('acceuil/register.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
